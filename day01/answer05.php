6873a95738fe44a88e7b836d741a478e1ee2e291
halland09 <pjs980720@gmail.com>
2025-04-30 14:30:19 +0900

    최종 최종 수정

--- day01/answer05.php
+++ day01/answer05.php
@@ -3,13 +3,13 @@
     $child = 5000;
     $num01 = 3;
     $adult = 8000;
-    $num01 = 2;
+    $num02 = 2;
 
-    $total = $child*$num01+$adult*$num02;
+    $total =  $child*$num01+$adult*$num02;
 
     echo "소인 입장료 : ".$child."원<br>";
     echo "소인 몇명 : ".$num01."명<br>";
     echo "대인 입장료 : ".$adult."원<br>";
-    echo "대인 몇명  : ".$num02."명<br>";
-    echo "총합  : ".$total."원<br>";
+    echo "대인 몇명 : ".$num02."명<br>";
+    echo "총합 : ".$total."원<br>";
 ?>

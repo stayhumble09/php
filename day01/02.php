1e7f5b5aca62660077cc24836f23b1da5fe253fd
halland09 <pjs980720@gmail.com>
2025-04-30 14:28:38 +0900

    다시 다시 수정

--- day01/02.php
+++ day01/02.php
@@ -1,55 +1,52 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>변수</title>
 </head>
-
 <body>
     <?php
-    $a = 10;
-    $b = 20;
-    $c = $a + $b;
-    echo $c;
-    //kor 점90  eng 80 math 85
-    // 총점 구하고 평균 구하고 출력
+        $a = 10;
+        $b = 20;
+        $c = $a + $b;
+        echo $c;
+        //kor 점90  eng 80 math 85
+        // 총점 구하고 평균 구하고 출력
     ?>
     <?php
-    $kor = 90;
-    $eng = 80;
-    $math = 85;
-    $sum = $kor + $eng + $math;
-    $avg = $sum / 3;
-    $num = -120.345;
-    $txt = "hello";
-    $txt02 = 'hello';
-    $x = 5 >= 3;
-    $y = 5 <= 3;
-
-    echo '<p>국어점수 : $kor</p>';
-    echo "<p>국어점수 : $kor</p>";
-    echo "<p>영어점수 : $eng</p>";
-    echo "<p>수학점수 : $math</p>";
-    echo "<p>총점 : $sum</p>";
-    echo "<p>평균 : $avg</p>";
-    var_dump($kor);
-    echo "<br>";
-    var_dump($txt);
-    echo "<br>";
-    var_dump($txt02);
-    echo "<br>";
-    var_dump($num);
-    echo "<br>";
-    var_dump($num);
-    echo "<br>";
-    var_dump($x);
-    echo "<br>";
-    var_dump($y);
-
+        $kor = 90;
+        $eng = 80;
+        $math = 85;
+        $sum  = $kor+$eng+$math;
+        $avg = $sum/3;
+        $num = -120.345;
+        $txt = "hello";
+        $txt02 = 'hello';
+        $x =  5 >= 3;
+        $y =  5 <= 3;
+        
+        echo '<p>국어점수 : $kor</p>';
+        echo "<p>국어점수 : $kor</p>";
+        echo "<p>영어점수 : $eng</p>";
+        echo "<p>수학점수 : $math</p>";
+        echo "<p>총점 : $sum</p>";
+        echo "<p>평균 : $avg</p>";
+        var_dump($kor);
+        echo "<br>";
+        var_dump($txt);
+        echo "<br>";
+        var_dump($txt02);
+        echo "<br>";
+        var_dump($num);
+        echo "<br>";
+        var_dump($num);
+        echo "<br>";
+        var_dump($x);
+        echo "<br>";
+        var_dump($y);
+        
     ?>
-
+    
 </body>
-
 </html>
--- day01/03.php
+++ day01/03.php
@@ -1,36 +1,32 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>php</title>
 </head>
-
 <body>
     <?php
-    $arr = array("빨강", "주황", "노랑");
-    var_dump($arr);
-    echo "<br>";
-    echo $arr[0];
+        $arr =  array("빨강","주황","노랑");
+        var_dump($arr);
+        echo "<br>";
+        echo $arr[0];
+        echo "<br>";
     ?>
     <?php
-    class Hello
-    {
-        public $name = "홍길동";
-        function sayHello()
-        {
-            return $this->name . "님 반갑습니다";
+        class Hello {
+            public $name ="홍길동";
+            function sayHello() {
+                return $this->name."님 반갑습니다";
+            }
         }
-    }
-    $msg = new Hello();
-    echo $msg->sayHello();
-    echo "<br>";
-    $myNull = NULL;
-    var_dump($myNull);
-    echo "<br>";
-    var_dump($msg);
+        $msg = new Hello();
+        echo $msg->sayHello();
+        echo "<br>";
+        var_dump($msg);
+        echo "<br>";
+        $myNull = NULL;
+        var_dump($myNull);
     ?>
 </body>
-
 </html>
